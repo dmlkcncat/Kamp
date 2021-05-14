b001a8323b254c025d34ddac971ed8addd2aeece
dmlkcncat <67742057+dmlkcncat@users.noreply.github.com>
2021-05-14 20:51:45 +0300

    Add files via upload

---
+++ FinalProject/MyFinalProject/ConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductTest();
+            //CategoryTest();
+            Console.ReadLine();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EFCategoryDal());
+
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+           
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EFProductDal());
+
+            foreach (var item in productManager.GetProductDetails())
+            {
+                Console.WriteLine(item.ProductName + "/" + item.CategoryName);
+            }
+        }
+    }
+}
---
+++ FinalProject/MyFinalProject/Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,23 @@
+﻿
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    //generic constaint --> class derken referans tip olabilir --> 
+    //IEntity veya IEntityden impelemente bir class olabilir
+    //newlenebilirdir, IEntity interface olduğundan newlenemez
+
+    public interface IEntityRepository <T> where T: class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filte);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+       
+    }
+}
