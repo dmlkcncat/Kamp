628190844b1eea3445cc0a35fa871616f94cd0b5
dmlkcncat <67742057+dmlkcncat@users.noreply.github.com>
2021-05-14 20:49:30 +0300

    Add files via upload

---
+++ Dictionary/Dictonary/Dictonary/MyDictonary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictonary
+{
+    class MyDictonary<T, Y>
+    {
+        T[] _keys;
+        Y[] _values;
+        T[] _tempKeys;
+        Y[] _tempValues;
+
+        public MyDictonary()
+        {
+            _keys = new T[0];
+            _values = new Y[0];
+        }
+
+        public void Add(T key, Y value)
+        {
+            T[] _tempKeys = _keys;
+            Y[] _tempValues = _values;
+
+            _keys = new T[_keys.Length + 1];
+            _values = new Y[_values.Length + 1];
+
+            for (int i = 0; i < _tempKeys.Length; i++)
+            {
+                _keys[i] = _tempKeys[i];
+            }
+
+            _keys[_keys.Length -1 ] = key;
+
+            for (int i = 0; i < _tempValues.Length; i++)
+            {
+                _values[i] = _tempValues[i];
+            }
+
+            _values[_values.Length - 1] = value;
+        }
+     
+    }
+}
---
+++ Dictionary/Dictonary/Dictonary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> sehirler = new Dictionary<string, int> ();
+            sehirler.Add("Ankara", 06);
+            sehirler.Add("Zonguldak", 67);
+
+            foreach (var item in sehirler)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(sehirler.Count);
+
+            MyDictonary<string, int> sehirler2 = new MyDictonary<string, int>();
+            sehirler2.Add("Istanbul", 34);
+            sehirler2.Add("Bolu", 14);
+            sehirler2.Add("Izmit", 41);
+
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
